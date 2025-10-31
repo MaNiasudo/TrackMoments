df0a447b2f11d1e0a3731f69643334ae2e82cee5
TahaEfazati <Wheremania@gmail.com>
2025-10-31 15:17:54 +0330

    added register

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import TaskList ,TaskDetail, TaskCreate ,TaskUpdate , TaskDelete , CustomLoginView 
+from .views import TaskList ,TaskDetail, TaskCreate ,TaskUpdate , TaskDelete , CustomLoginView , register
 
 from django.contrib.auth.views import LogoutView
 
 path('login/', CustomLoginView.as_view(), name='login'),
 path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-path('', )
+path('register/',register,name='register' )
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView , UpdateView ,DeleteView
@@ -6,7 +6,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView 
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin 
-
+from .forms import UserRegisterForm , UserUpdateForm
+from django.contrib import messages
 
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'
@@ -22,3 +23,13 @@ class UserProfileView(DetailView):
     model = User
     template_name = "users/profile.html"
     context_object_name = "profile_user"
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request, f"Your account has been created! You can login now")
+                return redirect('login')
