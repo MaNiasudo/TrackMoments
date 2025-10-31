a5b6d29f5ba036ce02a45c900ed3f87c59398493
TahaEfazati <Wheremania@gmail.com>
2025-10-31 15:05:14 +0330

    added forms urls and views for login register

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import TaskList ,TaskDetail, TaskCreate ,TaskUpdate , TaskDelete , CustomLoginView 
+
+from django.contrib.auth.views import LogoutView
+
+path('login/', CustomLoginView.as_view(), name='login'),
+path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+path('', )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView , UpdateView ,DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin 
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    fields = '__all__'
+    redredirect_authenticated_user = True
+    
+
+    def get_success_url(self):
+        return reverse_lazy('profile')
+    
+
+class UserProfileView(DetailView):
+    model = User
+    template_name = "users/profile.html"
+    context_object_name = "profile_user"
